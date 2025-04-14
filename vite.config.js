c917ba8ccbcfa90cedec97d522119204a5c04241
Sadia Sakharkar <sakharkarsadia@gmail.com>
2025-04-14 14:48:27 +0530

    Ready for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/kali-linux-portfolio/",
+  base: "/",
   plugins: [react()],
 });
